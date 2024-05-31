f58020accfbb803f37ec919f7f524bd68a27a08a
StarmiyaMiyuki <sousaku_0912@yahoo.co.jp>
2024-05-31 13:09:25 +0900

    m

--- pinecone/main.py
+++ pinecone/main.py
@@ -6,18 +6,18 @@ from db import PineconeClient
 from embedding import Embedding
 from data.kyoani import anime_title_dicts
 from util.csv import create_df_from_csv
+from embed import embed
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Initialize Pinecone client
-index_name = "kyoani-title-index"
-# namespace = "anime_titles"
-namespace = "apparels"
+index_name = "image-index"
+namespace = "kyoani_key_visuals"
 pc = PineconeClient(index_name=index_name)
 
 # Control Plane
-# pc.create_index()
+# pc.create_index(dimension=1024)
 # pc.delete_index()
 # pc.delete_vectors(namespace=namespace, delete_all=True)
 
@@ -25,15 +25,16 @@ pc = PineconeClient(index_name=index_name)
 # dicts = anime_title_dicts
 # ja_titles = [dict["ja"] for dict in dicts]
 # en_titles = [dict["en"] for dict in dicts]
-csv_path = os.path.abspath("./data/apparels.csv")
-df = create_df_from_csv(csv_path)
-df = df[:10]
-embeds = Embedding().get_embeddings(df["jp"].tolist())
-vectors = PineconeClient.vectors_from_embeds(df["en"].tolist(), embeds)
-pc.upsert_vectors(namespace=namespace, vectors=vectors)
+# csv_path = os.path.abspath("./data/upto_270000_apparels.csv")
+# df = create_df_from_csv(csv_path)
+# df = df[900:]
+# embeds = Embedding().get_embeddings(df["jp"].tolist())
+# vectors = PineconeClient.vectors_from_embeds(["Border"], [embed])
+# pc.upsert_vectors(namespace=namespace, vectors=vectors)
+# pc.delete_vectors(namespace=namespace, delete_all=True)
 
 # Query
-# query = "ぼっち・ざ・ろっく！"
+# query = "湊 あくあ フーディー"
 # query_embed = Embedding().get_embeddings([query])[0]
-# query_result = pc.query_index(namespace=namespace, vector=query_embed, top_k=5)
-# print(query_result)
+query_result = pc.query_index(namespace=namespace, vector=embed, top_k=5)
+print(query_result)
